c5f62ec7d1e47d75999cceed432d7a838c606f4b
ydmits <110300219+ydmits@users.noreply.github.com>
2025-07-11 00:27:32 +0300

    Add files via upload

---
+++ Lesson17.cpp
@@ -0,0 +1,106 @@
+﻿// Lesson17.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct user
+{
+    char name[20];
+    int age;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    // use C
+    int a = 10;
+    printf("a=%d\n", a);
+
+    // input data
+    puts("Enter a:");
+    scanf_s("%d", &a);
+    printf("a=%d\n", a);
+
+    // функции С
+    char str[128];
+    cin.ignore(cin.rdbuf()->in_avail());
+    puts("Enter string:");
+    gets_s(str, 128);
+    printf("str = %s\n", str);
+
+    // работа с текстовым файлом
+    FILE* fptr;
+    fopen_s(&fptr,"test.txt", "w");
+    fputs("Пробирается медведь сквозь густой валежник.\n", fptr);
+    fputs("Стали птицы громче петь. И расцвел подснежник.\n", fptr);
+    fclose(fptr);
+
+    // добавление информации в файл
+    fopen_s(&fptr, "test.txt", "a");
+    fputs("С.Я.Маршак\n", fptr);
+    fclose(fptr);
+
+    // чтение данных
+    fopen_s(&fptr, "test.txt", "r");
+    while (fgets(str, 128, fptr))
+    {
+        puts(str);
+    }
+    fclose(fptr);
+
+    // чтения файла посимвольно
+    char ch;
+    fopen_s(&fptr, "test.txt", "r");
+    while (!feof(fptr))
+    {   
+        ch = fgetc(fptr);
+        putchar(ch);
+    }
+    fclose(fptr);
+
+    // работа с двоичными файлами
+    a = 100;
+    float pi = 3.14;
+    fopen_s(&fptr, "test.dat", "w");
+    fprintf(fptr, "%d %f", a, pi);
+    fclose(fptr);
+
+    // чтение этого файла
+    int b;
+    float x;
+    fopen_s(&fptr, "test.dat", "r");
+    fscanf_s(fptr, "%d%f", &b, &x);
+    fclose(fptr);
+    printf("b = %d, x = %f", b, x);
+
+    // работа со структурами
+    user student1 = { "nick", 25 };
+    /*fopen_s(&fptr, "users.dat", "w");
+    fprintf(fptr, "%s %d", student1.name, student1.age);
+    fclose(fptr);
+
+    user student2;
+    fopen_s(&fptr, "users.dat", "r");
+    fscanf_s(fptr, "%s %d", &student2.name, &student2.age);
+    fclose(fptr);
+
+    printf("name - %s, age - %d", student2.name, student2.age);*/
+
+    fopen_s(&fptr, "users.dat", "w");
+    fwrite(&student1, sizeof(user), 1, fptr);
+    fclose(fptr);
+
+    user student2;
+    fopen_s(&fptr, "users.dat", "r");
+    fread(&student2, sizeof(user), 1, fptr);
+    fclose(fptr);
+    printf("name - %s, age - %d", student2.name, student2.age);
+
+
+   
+
+
+
+}
---
+++ miniBD.cpp
@@ -0,0 +1,115 @@
+﻿// miniBD.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct user
+{
+    char name[20];
+    int age;
+};
+
+void readBD(user* BD, int &size);
+void writeBD(user* BD, int size);
+void addBD(user* BD, int &size);
+void showBD(user* BD, int size);
+
+int main()
+{
+    // инициализация
+    user BD[100];
+    int size = 0;
+    
+    /* = {
+        { "alex", 18 },
+        { "max", 20 },
+        { "nick", 25 },
+        { "masha", 17 },
+        { "sveta", 22 }
+    };*/
+
+  
+
+    while (true)
+    {
+        system("cls");
+        cout << "Make your choice:" << endl;
+        cout << "1. Read BD" << endl;
+        cout << "2. Save BD" << endl;
+        cout << "3. Add into BD" << endl;
+        cout << "4. Show BD" << endl;
+        cout << "5. Exit" << endl;
+        int key;
+        cin >> key;
+        switch (key)
+        {
+            case 1: readBD(BD, size); break;
+            case 2: writeBD(BD, size); break;
+            case 3: addBD(BD, size); break;
+            case 4: showBD(BD, size); break;
+            case 5: exit(0);
+        }
+
+    }
+}
+
+
+void readBD(user* BD, int &size)
+{
+    FILE* fptr;
+    fopen_s(&fptr, "usersBD.dat", "r");
+
+    int i = 0;
+    while (fread(&BD[i], sizeof(user), 1, fptr))
+    {
+       //fread(&BD[i], sizeof(user), 1, fptr);
+       i++;
+       size++;
+    }
+    fclose(fptr);
+    cout << "Data was read successful!!!\n";
+    system("pause");
+
+}
+
+void writeBD(user* BD, int size)
+{
+    FILE* fptr;
+    fopen_s(&fptr, "usersBD.dat", "w");
+    for (int i = 0; i < size; i++)
+    {
+        fwrite(&BD[i], sizeof(user), 1, fptr);
+    }
+    fclose(fptr);
+    cout << "Data was saved successful!!!\n";
+    system("pause");
+
+
+}
+
+void addBD(user* BD, int &size)
+{
+    cout << "Enter user name:";
+    cin >> BD[size].name;
+    cout << "Enter user age:";
+    cin >> BD[size].age;
+    size++;
+}
+
+void showBD(user* BD, int size)
+{
+    system("cls");
+    cout << "========================\n";
+    cout << "|    name     |   age  |\n";
+    cout << "========================\n";
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "|" << setw(11)<< BD[i].name << "  |" << setw(8)<< BD[i].age << "|\n";
+        cout << "========================\n";
+    }
+    system("pause");
+}
